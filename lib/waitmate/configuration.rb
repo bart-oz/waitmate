409ba25eca659511a30b87a96d642478d6007926
Bart Ozdoba <bartek.ozdoba@gmail.com>
2026-07-04 09:28:35 +0200

    [WM-005][FEAT] Add gatekeeper controller concern

--- lib/waitmate/configuration.rb
+++ lib/waitmate/configuration.rb
@@ -2,13 +2,14 @@
 
 module Waitmate
   class Configuration
-    attr_accessor :adapter, :queue_ttl, :polling_interval, :ticket_ttl
+    attr_accessor :adapter, :queue_ttl, :polling_interval, :ticket_ttl, :waiting_room_path
 
     def initialize
       @adapter = :redis
       @queue_ttl = 300
       @polling_interval = 5
       @ticket_ttl = 120
+      @waiting_room_path = "/waitmate/room"
     end
   end
 end
